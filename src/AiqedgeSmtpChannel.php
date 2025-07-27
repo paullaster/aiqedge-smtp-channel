81dde70a0a1b2e0431026e948a705801146f641f
paullaster <paullaster>
2025-07-27 15:33:50 +0300

    refactor: inject API URL and key via constructor for improved testability and flexibility
    Store API credentials as class properties
    Use constructor injection instead of config() calls
    Update send method to use injected properties

--- src/AiqedgeSmtpChannel.php
+++ src/AiqedgeSmtpChannel.php
@@ -8,14 +8,46 @@ use Illuminate\Support\Facades\Log;
 
 class AiqedgeSmtpChannel
 {
+    /**
+     * The URL for the AIQEDGE-SMTP API.
+     *
+     * @var string
+     */
+    protected string $apiUrl;
+
+    /**
+     * The API key for the AIQEDGE-SMTP service.
+     *
+     * @var string
+     */
+    protected string $apiKey;
+
+    /**
+     * Create a new AIQEDGE-SMTP channel instance.
+     *
+     * @param string $apiUrl
+     * @param string $apiKey
+     * @return void
+     */
+    public function __construct(string $apiUrl, string $apiKey)
+    {
+        $this->apiUrl = $apiUrl;
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * Send the given notification.
+     *
+     * @param  mixed  $notifiable
+     * @param  \Illuminate\Notifications\Notification  $notification
+     * @return void
+     */
     public function send($notifiable, Notification $notification): void
     {
         $message = $notification->toAiqedgeSmtp($notifiable);
 
-        $apiKey = config('services.aiqedge_smtp.key');
-        $apiUrl = config('services.aiqedge_smtp.url');
-
-        $endpoint = "{$apiUrl}/{$apiKey}/send";
+        // Use the properties that were set in the constructor
+        $endpoint = "{$this->apiUrl}/{$this->apiKey}/send";
 
         $response = Http::post($endpoint, $message);
 
